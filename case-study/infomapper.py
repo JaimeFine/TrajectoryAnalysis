0a6cdbf721964d1e6224780fc4daef60719150dd
JaimeFine <jaimefine6@gmail.com>
2026-01-23 18:54:26 +0800

    modified:   case-study/infomapper.py

--- case-study/infomapper.py
+++ case-study/infomapper.py
@@ -1,3 +1,9 @@
+###############################################
+##                                           ##
+##                DEPRECATED                 ##
+##                                           ##
+###############################################
+
 import pandas as pd
 import numpy as np
 import networkx as nx
